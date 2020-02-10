08d7477f4ada1fc0fd6ee29874beb6478d2577d2
John <john.akonya@gmail.com>
2020-02-10 17:06:40 +0300

    entry point - index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = class MathsService {
+    
+    factorial(num) {
+        if (num < 0) 
+            return -1;
+        else if (num == 0) 
+            return 1;
+        else {
+            return (num * this.factorial(num - 1));
+        }
+    }
+};
+
+
+
